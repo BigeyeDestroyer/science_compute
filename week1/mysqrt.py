59cda209cb95448a08ac30bd9659148de965d3d0
BigeyeDestroyer <lurui08@163.com>
2015-03-20 15:38:49 +0800

    add debug flag, return value

--- week1/mysqrt.py
+++ week1/mysqrt.py
@@ -2,16 +2,21 @@
 Module for approximating square
 More...
 """
-def sqrt2(x):
+def sqrt2(x, debug = False):
+    """
+    more details 
+    """
     s = 1.
     kmax = 100 
     tol = 1.e-14
     for k in range(kmax + 1):
-        print "Before iteration %s, s = %20.15f" %(k, s) 
+        if debug:
+            print "Before iteration %s, s = %20.15f" %(k, s) 
         s0 = s
         s = 0.5 * (s + x / s)
         delta_s = s - s0
         if abs(delta_s / x) < tol:
             break 
-    print "After %s iterations, s = %20.15f" %(k+1, s)
-
+    if debug:
+        print "After %s iterations, s = %20.15f" %(k+1, s)
+    return s
