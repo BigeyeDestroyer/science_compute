48fc2ea5c72344dd01e162f9e1ff7b1992df0686
BigeyeDestroyer <lurui08@163.com>
2015-03-23 20:14:44 +0800

    added tests of inputs

--- week1/mysqrt.py
+++ week1/mysqrt.py
@@ -6,10 +6,19 @@ def sqrt2(x, debug = False):
     """
     more details 
     """
+    from numpy import nan
+    
+    if x == 0.:
+        return 0.
+    elif x < 0:
+        print "*** Error, x must be nonnegative"
+        return nan
+    assert x > 0., "Should not get here!"
+
     s = 1.
     kmax = 100 
     tol = 1.e-14
-    for k in range(kmax + 1):
+    for k in range(kmax):
         if debug:
             print "Before iteration %s, s = %20.15f" %(k, s) 
         s0 = s
